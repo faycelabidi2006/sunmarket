28fce10e0f789135a31875937ed49335a635fa9e
Faycel Abidi <faycel.abidi@moe.sch.ae>
2026-05-14 11:24:13 +0400

    new supabase

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://tysnbikyhznxzbdaaxsq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5c25iaWt5aHpueHpiZGFheHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDgxNTcsImV4cCI6MjA5MzQyNDE1N30.jpnJtpVTkUZm2fymGFTFzrIOOhTYl56ish_ps4tHiTA'
+  'https://xhyisfvtfumqvddyvxrb.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoeWlzZnZ0ZnVtcXZkZHl2eHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MzI4MjMsImV4cCI6MjA5NDMwODgyM30.8_dF2UocnNuJT-yIDO8ihi1gLFTQBxn0rrrnk3r-klY'
 )
